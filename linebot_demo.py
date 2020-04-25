7dd7e617025a8ab4e9e16aa576cd7e239dde0b14
ghnmqdtg <ghnmqdtg@gmail.com>
2020-04-25 14:58:54 +0800

    update reply message

--- linebot_demo.py
+++ linebot_demo.py
@@ -44,11 +44,29 @@ def callback():
 # handling message
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    '''
     print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
     content = "{}: {}".format(event.source.user_id, event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=content))
+    '''
+
+    print(event)
+    text = event.message.text
+
+    if (text == "Hi"):
+        reply_text = "Hello"
+    elif(text == "你好"):
+        reply_text = "哈囉"
+    elif(text == "欸"):
+        reply_text = "欸三小 閉嘴"
+    else:
+        reply_text = "你家失火啦"
+
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
