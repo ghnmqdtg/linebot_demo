362e0136bfb9ddb5993f549e02b92dd94319ba3e
ghnmqdtg <ghnmqdtg@gmail.com>
2020-05-05 13:14:09 +0800

    Add Current Weather function

--- linebot_demo.py
+++ linebot_demo.py
@@ -11,11 +11,46 @@ from linebot.models import (
 )
 
 import os
-
+import requests
+import urllib.parse
 import configparser
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+
+class GetWeather:
+    def __init__(self, functions, location):
+        self.functions = functions
+        self.location = location
+
+    def GetCurrent(self):
+        # URL for fetching the current weather
+        Request_URL = config["URL"]["observation_auto"]\
+            + config["settings"]["Authorization"] + "&format=JSON"\
+            + "&locationName=" + urllib.parse.quote(self.location)
+
+        # filter out which data we need
+        data = requests.get(Request_URL).json()["records"]["location"]
+
+        # examine if the location is valid
+        if not data:
+            WeatherData = "target station not found"
+            # print(target_station)
+            return False
+
+        # load message
+        # ref: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
+        CITY = data[0]["parameter"][0]["parameterValue"]
+        TOWN = data[0]["parameter"][2]["parameterValue"]
+        WeatherData = data[0]["weatherElement"]
+
+        msg = CITY + TOWN + " " + self.functions + "\n\n"
+        msg += "氣溫：" + WeatherData[3]["elementValue"] + "℃\n"
+        msg += "濕度：" + \
+            str(round(float(WeatherData[4]["elementValue"]) * 100)) + "% RH\n"
+        msg += "風向：" + WeatherData[1]["elementValue"] + "°\n"
+        msg += "風速：" + WeatherData[0]["elementValue"] + " m/s\n"
+        return msg
 
 app = Flask(__name__)
 
@@ -61,10 +96,16 @@ def handle_message(event):
         reply_text = "Hello"
     elif(text == "你好"):
         reply_text = "哈囉"
-    elif(text == "欸"):
-        reply_text = "幹嘛"
+    elif("目前天氣" in text):
+        functions = "目前天氣"
+        location = text[5:]
+        if location:
+            reply_text = GetWeather(functions, location).GetCurrent()
+        else:
+            reply_text = "未輸入觀測站名稱"
     else:
         reply_text = "嘿"
+
 
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
